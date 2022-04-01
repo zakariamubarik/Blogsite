8761ffe5eb63f06fa4f5f150b57fe999980397b6
Zakaria Mubarik <101276169+zakariamubarik@users.noreply.github.com>
2022-04-01 09:33:20 +0000

    react route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react'
-import Home from './pages/Home'
-import {Routes,Route} from "react-router-dom"
+import Home from'./pages/Home'
+import {Routes,Route, Navigate} from "react-router-dom"
+import About from './pages/About'
+import Blog from './pages/Blog'
 
 function App() {
   return (
     <Routes>
-   <Route path="/" element={<Home/>} />
-   
+     <Route path="/" element={<Home/>} />
+     <Route path="/about" element={<About/>} />
+     <Route path="/blog" element={<Blog/>} />
+     <Route path="*" element={<Navigate to="/"/>} />
+     
+
     </Routes>
   )
 }
--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import withLayout from './withLayout'
 
-function Home() {
+function Blog() {
   return (
     <div>
-    <h1>welcome to my Homepage</h1>
+    <h1>welcome to my BlogPage</h1>
     <div>
     <p>10 Sept 2021 — WhatsApp Web is a free version of the famous chat/messaging app WhatsApp that will allow you to chat with your contacts from your computert default Home. </p>
 
@@ -20,5 +20,5 @@ function Home() {
   )
   }
 
-export default withLayout(Home)
+export default withLayout(Blog)
 
